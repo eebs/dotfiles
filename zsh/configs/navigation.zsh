eeb572dfbd696f6e840224bd5aaa02ea85cb2895
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2018-11-16 17:25:57 -0500

    Fast project switching
    This would be better if it could somehow take into account any folder in
    `~/code/*/`. Maybe I need to glob `~/code/*` and add those to my cdpath?
    Or use a more static list, rather than the cdpath directories.

---
+++ zsh/configs/navigation.zsh
@@ -0,0 +1,38 @@
+# with cdpath enabled, auto_cd gives too many false positives
+unsetopt auto_cd
+
+cdpath=(
+  $HOME/code \
+  $HOME/code/eebs \
+  $HOME/code/work/current \
+  $HOME
+)
+
+_cdpath_directories() {
+  modified_in_last_days=${1:-999}
+  echo "${CDPATH//:/\n}" | while read dir; do
+    find -L "$dir" \
+      -not -path '*/\.*' \
+      -type d \
+      -atime -"$modified_in_last_days" \
+      -maxdepth 1
+  done
+}
+
+_is_a_git_repo() {
+  while read dir; do
+    if [[ -d "$dir/.git" ]]; then
+      basename "$dir"
+    fi
+  done
+}
+projects() {
+  _cdpath_directories $1 | _is_a_git_repo
+}
+
+tm-select-session() {
+  project=$(projects | fzf --reverse)
+  if [ ! -z "$project" ]; then
+    (cd "$project" && tat)
+  fi
+}
