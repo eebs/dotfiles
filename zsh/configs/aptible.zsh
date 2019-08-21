eeb591f0d029a3d3c49e49958c4a6d95ddffa0fe
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2019-08-21 16:11:02 -0400

    Adds aptible_tunnel function

--- zsh/configs/aptible.zsh
+++ zsh/configs/aptible.zsh
@@ -1,3 +1,7 @@
 with_tunnel () {
   DATABASE_URL=$(tmux capture-pane -t 1 -p | grep postgresql:// | tail -1 | cut -d " " -f 3) $@
 }
+
+function aptible_tunnel() {
+  aptible db:list | fzf --reverse | xargs aptible db:tunnel;
+}
