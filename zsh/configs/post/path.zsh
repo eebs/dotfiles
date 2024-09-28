eeb51923ae020cef514d2f551e2e86fdc549166f
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2024-09-28 14:06:17 -0400

    Postgres in path

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -13,4 +13,6 @@ PATH="$HOME/.bin:$PATH"
 # Add project bin paths if they are trusted
 PATH=".git/safe/../../bin:$PATH"
 
+PATH="/Applications/Postgres.app/Contents/Versions/16/bin:$PATH"
+
 export -U PATH
