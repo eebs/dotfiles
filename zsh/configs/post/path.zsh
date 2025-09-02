eeb5ffebaac438ad29fdcf1fc3255558293c623d
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2025-09-02 15:38:43 -0400

    Java vars

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -4,6 +4,15 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Load asdf
 PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
+# Set JAVA_HOME and JDK_HOME
+local java_path
+java_path="$(asdf which java)"
+if [[ -n "${java_path}" ]]; then
+  export JAVA_HOME
+  JAVA_HOME="$(dirname "$(dirname "${java_path:A}")")"
+  export JDK_HOME=${JAVA_HOME}
+fi
+
 # Add global npm path
 PATH="$(npm config get prefix)/bin:$PATH"
 
