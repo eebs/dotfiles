eeb5c782d89f7b9fd0bd7a77445050e4e45dfaa3
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2025-09-03 11:11:59 -0400

    Set a bunch of options from old config

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,10 +1,32 @@
 -- vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 -- vim.g.lazyvim_ruby_formatter = "standardrb"
 
+vim.cmd("set nobackup")
+vim.cmd("set nowritebackup")
+vim.cmd("set noswapfile")
+vim.cmd("set ruler")
+vim.cmd("set showcmd")
+vim.cmd("set incsearch")
+vim.cmd("set laststatus=2")
+vim.cmd("set autowrite")
+
+vim.cmd("set scrolloff=4")
+
+vim.cmd("set updatetime=100")
+
 vim.cmd("set expandtab")
+vim.cmd("set shiftround")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.cmd("set textwidth=80")
+vim.cmd("set colorcolumn=+1")
+
+vim.cmd("set splitbelow")
+vim.cmd("set splitright")
+
+vim.cmd("set cursorline")
 
 vim.opt.swapfile = false
 
@@ -22,6 +44,13 @@ vim.keymap.set('n', 'k', 'gk')
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('i', 'kj', '<esc>')
 vim.keymap.set('i', 'jj', '<esc>')
+
+-- Write file
+vim.keymap.set('n', '<leader>j', ':w<cr>')
+
+-- Jump to first non-whitespace character insteaad of start of line
+vim.keymap.set('n', '0', '^', { noremap = true })
+vim.keymap.set('n', '^', '0', { noremap = true })
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
