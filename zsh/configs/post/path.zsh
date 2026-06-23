eeb514d186d42963fc5856c4c9d85ec2608d7ceb
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2026-06-23 13:03:02 -0400

    Configure mise

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,27 +1,16 @@
-# Load homebrew
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Postgres.app
+PATH="/Applications/Postgres.app/Contents/Versions/19/bin:$PATH"
 
-# Load asdf
-PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-
-# Set JAVA_HOME and JDK_HOME
-local java_path
-java_path="$(asdf which java)"
-if [[ -n "${java_path}" ]]; then
-  export JAVA_HOME
-  JAVA_HOME="$(dirname "$(dirname "${java_path:A}")")"
-  export JDK_HOME=${JAVA_HOME}
-fi
-
-# Add global npm path
-PATH="$(npm config get prefix)/bin:$PATH"
-
-# Add dotfiles bin path
-PATH="$HOME/.bin:$PATH"
+# Add dotfiles before everything else
+PATH="$HOME/.bin:$HOME/.local/bin:$PATH"
 
 # Add project bin paths if they are trusted
 PATH=".git/safe/../../bin:$PATH"
 
-PATH="/Applications/Postgres.app/Contents/Versions/18/bin:$PATH"
-
 export -U PATH
+
+# Load homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Load Mise
+eval "$(~/.local/bin/mise activate zsh)"
