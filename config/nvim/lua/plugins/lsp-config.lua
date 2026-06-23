eeb5b6dc7a2cafd1ed19f2adb29ad28b649d4e60
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2026-06-23 13:02:47 -0400

    Remove standardrb lsp

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -2,11 +2,11 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "ruby_lsp", "standardrb" },
+      ensure_installed = { "lua_ls", "ruby_lsp" },
     },
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
     },
-  }
+  },
 }
